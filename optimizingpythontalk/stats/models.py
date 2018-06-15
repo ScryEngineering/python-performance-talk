092e5e1db6656eab2bf753e124e1eebcaa24ad6d
Janis Lesinskis <janis@lesinskis.com>
2018-06-15 20:35:15 +1000

    Define some models

--- optimizingpythontalk/stats/models.py
+++ optimizingpythontalk/stats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Repository(models.Model):
+    """Represent a repository"""
+    repo_url = models.CharField(max_length=30)
+    repo_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.repo_name
+
+class CommitCounts(models.Model):
+    """Respresent commit counts by author for a repo"""
+    author_name = models.CharField(max_length=30)
+    commit_count = models.IntegerField()
+    repo = models.ForeignKey(
+        Repository,
+        on_delete=models.CASCADE,
+        verbose_name="the related repository",
+    )
+
+    def __str__(self):
+        return "{} - {} commits".format(self.author_name, self.commit_count)
