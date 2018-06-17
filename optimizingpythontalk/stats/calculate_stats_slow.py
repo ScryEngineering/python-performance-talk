5c1791875abc7b4d603c0f43f6cf0e450c7585cd
Janis <janis@jaggedverge.com>
2018-06-18 01:43:04 +1000

    Move variable to global scope

--- optimizingpythontalk/stats/calculate_stats_slow.py
+++ optimizingpythontalk/stats/calculate_stats_slow.py
@@ -4,18 +4,17 @@ def recursive_average(items, cumulative_sum=0, count=0):
     """Compute the mean (average) for an iterable"""
     if not items:
         return cumulative_sum / count
-    else:
-        return recursive_average(items[1:], cumulative_sum+items[0], count+1)
+    return recursive_average(items[1:], cumulative_sum+items[0], count+1)
 
+authors_plus_commit_info = []
 def averages_with_threshold(threshold: int) -> dict:
     """Threshold is the minimum number of commits for inclusion"""
-    authors_plus_commit_info = []
+    authors_plus_commit_info.clear()
     for commit_info in CommitCounts.objects.all():
         if commit_info.commit_count > threshold:
             authors_plus_commit_info.append(
                 (commit_info.author_name, commit_info.commit_count)
             )
-
     authors = {}
     for item in authors_plus_commit_info:
         try:
