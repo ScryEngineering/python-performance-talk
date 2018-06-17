c064ed5b77638d9d22a54cf30d5163897091f0ed
Janis <janis@jaggedverge.com>
2018-06-18 00:43:11 +1000

    Create test case for averages calculation

--- optimizingpythontalk/stats/tests.py
+++ optimizingpythontalk/stats/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Repository, CommitCounts
+
+class StatsTestCase(TestCase):
+    def setUp(self):
+        a = Repository.objects.create(repo_url="a.example.com", repo_name="a")
+        b = Repository.objects.create(repo_url="b.example.com", repo_name="b")
+
+        CommitCounts.objects.create(author_name="bob", repo=a, commit_count=10)
+        CommitCounts.objects.create(author_name="bob", repo=b, commit_count=2)
+        CommitCounts.objects.create(author_name="alice", repo=a, commit_count=20)
+        CommitCounts.objects.create(author_name="alice", repo=b, commit_count=20)
+        CommitCounts.objects.create(author_name="eve", repo=b, commit_count=10)
+
+    def test_pure_averages(self):
+        """test that a threshold of zero gives the average"""
+        from .calculate_stats_slow import averages_with_threshold
+        results = averages_with_threshold(0)
+        assert len(results) == 3 # No authors filtered out
+        assert results["bob"] == (10+ 2 / 2)
+        assert results["alice"] == (20 + 20 / 2)
+        assert results["eve"] == 10
