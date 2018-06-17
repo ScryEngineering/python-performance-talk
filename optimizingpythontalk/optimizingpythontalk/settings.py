dff0d777f4442a70c6faede323bb8633f602592d
Janis <janis@jaggedverge.com>
2018-06-18 00:42:31 +1000

    Set up DB

--- optimizingpythontalk/optimizingpythontalk/settings.py
+++ optimizingpythontalk/optimizingpythontalk/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stats',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST' : 
+            {
+                'NAME': 'test_db',
+            }
     }
 }
 
