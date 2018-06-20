62a306fe8335e10371cecc2eea6c391bd3a06edb
Janis <janis@jaggedverge.com>
2018-06-21 02:22:09 +1000

    install django silk for profiling

--- optimizingpythontalk/optimizingpythontalk/settings.py
+++ optimizingpythontalk/optimizingpythontalk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stats',
+    'silk',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'optimizingpythontalk.urls'
