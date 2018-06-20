80cf46936cec4ee0a213d3d6fa15f3de562c2aae
Janis <janis@jaggedverge.com>
2018-06-21 02:25:21 +1000

    Added in some profiling

--- optimizingpythontalk/stats/calculate_stats.py
+++ optimizingpythontalk/stats/calculate_stats.py
@@ -1,5 +1,14 @@
+import statistics
+
 from .models import Repository, CommitCounts
 
+from silk.profiling.profiler import silk_profile
+
+@silk_profile(name='faster average')
 def faster_average(items):
     """Compute the mean (average) for an iterable"""
     return sum(items) / len(items)
+
+@silk_profile(name='Standard library average')
+def std_lib_average(items):
+    return statistics.mean(items)
