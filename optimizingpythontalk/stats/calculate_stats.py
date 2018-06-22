827f2efa22a8cd285dff963bf62d0a3ddccfd54d
Janis <janis@jaggedverge.com>
2018-06-22 19:28:19 +1000

    Added explanatory docstring

--- optimizingpythontalk/stats/calculate_stats.py
+++ optimizingpythontalk/stats/calculate_stats.py
@@ -11,6 +11,8 @@ def std_lib_average(items):
     return statistics.mean(items)
 
 def averages_with_threshold_fast(threshold: int) -> dict:
+    """This attempts to be faster than the "slow" version of the code,
+    But doesn't have especially good performance due to a not so great query"""
     commit_counts = defaultdict(list)
     for commit_info in CommitCounts.objects.filter(commit_count__gt=threshold):
         commit_counts[commit_info.author_name].append(commit_info.commit_count)
